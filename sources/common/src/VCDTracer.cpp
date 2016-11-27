2aa2237760d20ec94c22facde1fe5b655f3ca795
WojciechRynczuk <wojciech.rynczuk@wp.pl>
2016-11-27 12:53:13 +0100

    New release name

--- sources/common/src/VCDTracer.cpp
+++ sources/common/src/VCDTracer.cpp
@@ -63,7 +63,7 @@ void TRACER::VCDTracer::GenerateBasicInformation()
 {
     DumpLine("$date December 8, 2014 14:15:00");
     DumpLine("$end");
-    DumpLine("$version VCD Tracer \"Matylda\" Release v." + std::string(VERSION::STRING));
+    DumpLine("$version VCD Tracer \"Mateusz\" Release v." + std::string(VERSION::STRING));
     DumpLine("$end");
     DumpLine("$timescale 1 " + m_rSignalDb.GetTimeUnit());
     DumpLine("$end");
